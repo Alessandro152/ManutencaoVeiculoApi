ce4918e67d165f1670708a831aacc091b83f4a7e
Alessandro Silva <Alessandro.Silva@wolterskluwer.com>
2021-11-09 16:44:03 -0300

    Inclusao de classe configuration

---
+++ Infra/Context/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManutencaoVeiculoApi.Infra.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataBaseContext).Assembly);
+        }
+    }
+}
